54f3423ab3df67659f76752a5192f51a849a3078
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-04-22 11:40:52 +0900

    円を追加

---
+++ SwiftUITextbookChapter3/SwiftUITextbookChapter3/SwiftUIView.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftUIView.swift
+//  SwiftUITextbookChapter3
+//
+//  Created by home on 2020/04/22.
+//  Copyright © 2020 Swift-beginners. All rights reserved.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Circle()
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
